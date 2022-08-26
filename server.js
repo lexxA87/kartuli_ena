3a7727d2dbeb2be1f6f6dff6cedeacc93ed030a3
lexxA87 <alex.o.nikonov@gmail.com>
2022-08-26 23:51:09 +0400

    Added graphQL to server

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 // const mongoose = require("mongoose");
 
 const path = require("path");
@@ -10,16 +12,37 @@ const cors = require("cors");
 const PORT = process.env.PORT || config.get("serverPort");
 // const DB_URL = config.get("MongoDBUrl");
 
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => {
+    return "Hello world!";
+  },
+};
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(__dirname));
-app.use(express.static(path.resolve(__dirname, "build")));
+// app.use(express.static(__dirname));
+// app.use(express.static(path.resolve(__dirname, "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 // mongoose
 //   .connect(DB_URL, { useNewUrlParser: true })
