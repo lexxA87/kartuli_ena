22a17fed0970bca7491ffec135deef3b12c1ac74
lexxA87 <alex.o.nikonov@gmail.com>
2022-09-04 00:19:38 +0400

    Deploy to heroku 8

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.static(__dirname));
 app.use(express.static(path.resolve(__dirname, "build")));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
